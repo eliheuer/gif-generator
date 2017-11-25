31038e39e2701c2499c0ed279b1f9360dd88ee43
Eli Heuer <eliheuer@gmail.com>
2017-11-25 04:18:32 -0500

    New modes + docs

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,126 @@
+//View image with: mtpaint -v target/output.gif &> /dev/null
+
 extern crate rand;
 extern crate gif;
-
 use rand::Rng;
 use gif::{Frame, Encoder, Repeat, SetParameter};
-
 use std::io;
 use std::fs::File;
 use std::borrow::Cow;
 
-// Main
 fn main() {
-    
-    // Intro/welcome
-    println!("Welcome to GIF generator 0.1.0!");
+
+    // Intro/welcome:
+    println!("\n\n\n\nWelcome to GIF generator 0.1.0!");
     println!("*******************************\n");
-
-    // Random number:
-    let rand_num_test: u32 = rand::thread_rng().gen_range(1, 101);
-    println!("Random Number = {}", rand_num_test);
-
-    // Menu loop
+    
+    // TODO figure out how type casting works in rust:
+    // Menu loop:
     loop {
-        println!("Please input a size for width & height: ");
-        let mut img_size = String::new();
-        io::stdin().read_line(&mut img_size)
+        println!("Please input an image size:");
+        let mut img_size_test = String::new();
+        io::stdin().read_line(&mut img_size_test)
             .expect("Failed to read line");
-        let img_size: u16 = match img_size.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        println!("Image will be {}px X {}px...", img_size, img_size);
+        // let my_int: u16 = img_size_test.parse().unwrap();
+        println!("img_size_test{}",img_size_test);
         break;
     }
-    
-    // Output a new gif file
-    output_gif(4);
+
+    // Output a new gif file:
+    output_gif(16);
 }
 
+// Output gif based on function arguments:
 fn output_gif(img_size: u16) {
 
-    // Static array:
-    let array_1: [u8; 16] = [
-        0, 0, 0, 0,
-        0, 0, 0, 0,
-        0, 0, 0, 0,
-        0, 0, 0, 0,
+    println!("img_size: {}", img_size);
+
+    // Static array://///////////////////////////////////////////////
+    let _array_1: [u8; 64] = [
+        1, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0, 0, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 0, 0, 0,
+        0, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 0,
+        0, 0, 0, 0, 0, 0, 0, 1,
     ];
 
-    // Dynamic array:
-    let mut array_2: [u8; 16] = Default::default();
-    for i in 0..16 {
-        let mut rand_num: u8 = rand::thread_rng().gen_range(0, 2);
-        println!("RN: {}", rand_num);
-        array_2[i] = rand_num as u8;
+    // Initialize random array://////////////////////////////////////
+    let mut rand_col: [u8; 256];
+    let mut rand_col = [0; 256];
+
+    // Iterate over array, add random pixels: 
+    for i in 0..256 {
+        let rand_num: u8 = rand::thread_rng().gen_range(2, 9);
+        print!("{}", rand_num);
+        rand_col[i] = rand_num as u8;
     }
-    println!("\nArray 2: ");
-    println!("{:?}", array_2);
-    
-    // Set a color for earch array index number:
-    let color_map = &[
-        0xFF, 0xFF, 0xFF, // Color 0
-        0xFF, 0x00, 0x00, // Color 1
+
+    // Initialize array A:///////////////////////////////////////////
+    let mut ary_a: [u8; 256];
+    let mut ary_a = [0; 256];
+
+    // Iterate over array: 
+    for i in 0..256 { 
+        let rand_num: u8 = rand::thread_rng().gen_range(0, 9);
+        if i % 3 == 0 {
+            ary_a[i] = 6 as u8;
+        } else if i % 3 != 0 {
+            ary_a[i] = rand_num as u8;          
+        } else {
+            ary_a[i] = 0 as u8;
+        }
+    } 
+
+    // Initialize array B:///////////////////////////////////////////
+    let mut ary_b: [u8; 256];
+    let mut ary_b = [0; 256];
+
+    // Iterate over array: 
+    let mut count = 3;
+    for i in 0..256 { 
+        let rand_num: u8 = rand::thread_rng().gen_range(1, 3);
+        
+        if i == 0 { 
+            ary_b[i] = count as u8;
+        } else if i % 3 == 0 {
+            ary_b[i] = count as u8;
+        } else {
+            ary_b[i] = rand_num as u8;
+        }
+
+        if i % 32 == 0 {
+            count += 1;
+        }
+    }
+
+    // Define colors for map index
+    let col_map = &[
+        0xDD, 0xDD, 0xDD, // Color 0: White
+        0x55, 0x55, 0x55, // Color 1: Grey
+        0x40, 0x40, 0x40, // Color 2: Dark Grey
+        0xF0, 0x28, 0x28, // Color 3: Red
+        0xFF, 0x80, 0x00, // Color 4: Orange
+        0xF4, 0xD0, 0x3F, // Color 5: Yellow
+        0x27, 0xAE, 0x60, // Color 6: Green
+        0x34, 0x98, 0xDB, // Color 7: Blue
+        0x9B, 0x59, 0xB6, // Color 8: Purple
+        0xF0, 0x28, 0x28, // Color 3: Red
+        0xFF, 0x80, 0x00, // Color 4: Orange
+        0xF4, 0xD0, 0x3F, // Color 5: Yellow
+        0x27, 0xAE, 0x60, // Color 6: Green
+        0x34, 0x98, 0xDB, // Color 7: Blue
+        0x9B, 0x59, 0xB6, // Color 8: Purple
     ];
     
+    // Make output file
     let (width, height) = (img_size, img_size);
-    let gif_states = [array_2, array_1];
-
+    let gif_states = [ary_a, ary_b, rand_col];
     let mut image = File::create("target/output.gif").unwrap();
-    let mut encoder = Encoder::new(&mut image, width, height, color_map).unwrap();
+    let mut encoder = 
+        Encoder::new(&mut image, width, height, col_map).unwrap();
     encoder.set(Repeat::Infinite).unwrap();
     for state in &gif_states {
         let mut frame = Frame::default();
